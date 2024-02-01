a3a386b0321d6b1a1060390a32b67e5cabb3978f
Darpeex <foxyyysp@mail.ru>
2024-02-01 14:47:08 +0300

    feat: пробный график 1 + комментарии

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,28 @@
 import './App.css';
+import { LGraph, LiteGraph, LGraphCanvas } from 'litegraph.js';
 
 function App() {
+  const graph = new LGraph(); // создаём граф
+  const canvas = new LGraphCanvas('#mycanvas', graph); // Создаём холст и передаём html-элемент и graph в параметры
+
+  const node_const = LiteGraph.createNode('basic/const'); // Создаём узел с постоянным числом
+  node_const.pos = [200, 200]; // Определяем его координаты X, Y
+  graph.add(node_const); // Добавляем этот узел в графы, которые отображаются на холсте
+  node_const.setValue(4.5); // Задаём постоянное значение
+
+  const node_watch = LiteGraph.createNode('basic/watch'); // Создаём узел, который отображает входные данные
+  node_watch.pos = [700, 200]; // Определяем его координаты X, Y
+  graph.add(node_watch); // Добавляем этот узел в графы, которые отображаются на холсте
+
+  node_const.connect(0, node_watch, 0); // Узел с константой соединяем с отображающим узлом
+
+  graph.start(); // Запускаем график
+
   return (
     <div className="App">
-      <div className="page"></div>
+      <div className="page">
+        <canvas id="mycanvas" width="1024" height="720"></canvas>
+      </div>
     </div>
   );
 }

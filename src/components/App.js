f53c49bf667b9cb9ce5d5ac5917da9bd8b5e3a53
Darpeex <foxyyysp@mail.ru>
2024-02-02 11:54:48 +0300

    fix: useEffect и перемещение при перезагрузке

--- src/components/App.js
+++ src/components/App.js
@@ -1,25 +1,25 @@
 import './App.css';
 import { useEffect } from 'react';
-import { LGraph, LiteGraph, LGraphCanvas } from 'litegraph.js';
+import { LGraph, LiteGraph, LGraphNode, LGraphCanvas } from 'litegraph.js';
 
 function App() {
-  const graph = new LGraph(); // создаём граф
   useEffect(() => {
+    const graph = new LGraph(); // создаём граф
     const canvas = new LGraphCanvas('#mycanvas', graph); // Создаём холст и передаём html-элемент и graph в параметры
+
+    const node_const = LiteGraph.createNode('basic/const'); // Создаём узел с постоянным числом
+    node_const.pos = [200, 200]; // Определяем его координаты X, Y
+    graph.add(node_const); // Добавляем этот узел в графы, которые отображаются на холсте
+    node_const.setValue(4.5); // Задаём постоянное значение
+
+    const node_watch = LiteGraph.createNode('basic/watch'); // Создаём узел, который отображает входные данные
+    node_watch.pos = [700, 200]; // Определяем его координаты X, Y
+    graph.add(node_watch); // Добавляем этот узел в графы, которые отображаются на холсте
+
+    node_const.connect(0, node_watch, 0); // Узел с константой соединяем с отображающим узлом
+
+    graph.start(); // Запускаем график
   }, []);
-
-  const node_const = LiteGraph.createNode('basic/const'); // Создаём узел с постоянным числом
-  node_const.pos = [200, 200]; // Определяем его координаты X, Y
-  graph.add(node_const); // Добавляем этот узел в графы, которые отображаются на холсте
-  node_const.setValue(4.5); // Задаём постоянное значение
-
-  const node_watch = LiteGraph.createNode('basic/watch'); // Создаём узел, который отображает входные данные
-  node_watch.pos = [700, 200]; // Определяем его координаты X, Y
-  graph.add(node_watch); // Добавляем этот узел в графы, которые отображаются на холсте
-
-  node_const.connect(0, node_watch, 0); // Узел с константой соединяем с отображающим узлом
-
-  graph.start(); // Запускаем график
 
   return (
     <div className="App">

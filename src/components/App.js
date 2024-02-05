6ca9ad7290348487b803dacdd820911624e58b2e
Darpeex <foxyyysp@mail.ru>
2024-02-05 11:29:03 +0300

    refactor: изменена структура

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,6 @@
-import './App.css';
-import Header from './Header/Header';
-import Main from './Main/Main';
-import Footer from './Footer/Footer';
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
   return (
--- src/components/Header/Header.jsx
+++
@@ -1,7 +0,0 @@
-import './Header.css';
-
-function Header() {
-  return <header className="Header"></header>;
-}
-
-export default Header;

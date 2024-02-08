ac0a230cb6c611a2f478e8e933d5bef47d2502db
Darpeex <foxyyysp@mail.ru>
2024-02-08 14:45:13 +0300

    perf: убран closeMenu из функций

--- src/components/nodes/functions.js
+++ src/components/nodes/functions.js
@@ -2,43 +2,37 @@ import { LiteGraph } from 'litegraph.js';
 import { graph } from '../Main';
 
 // Задаётся постоянное число
-export const handleMountConstantNumberBlock = (closeMenu) => {
+export const handleMountConstantNumberBlock = () => {
   const node_constNumber = LiteGraph.createNode('basic/constNumber'); // Создаём узел с постоянным числом
   graph.add(node_constNumber); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };
 
 // Вывод результата
-export const handleMountResultBlock = (closeMenu) => {
+export const handleMountResultBlock = () => {
   const node_watch = LiteGraph.createNode('basic/result'); // Создаём узел, который отображает входные данные
   graph.add(node_watch); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };
 
 // Сложение
-export const handleMountAdditionalBlock = (closeMenu) => {
+export const handleMountAdditionalBlock = () => {
   const node_addition = LiteGraph.createNode('basic/addition'); // Создаём узел, который отображает входные данные
   graph.add(node_addition); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };
 
 // Вычитание
-export const handleMountSubstractionBlock = (closeMenu) => {
+export const handleMountSubstractionBlock = () => {
   const node_substraction = LiteGraph.createNode('basic/substraction'); // Создаём узел, который отображает входные данные
   graph.add(node_substraction); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };
 
 // Умножение
-export const handleMountMultiplicationBlock = (closeMenu) => {
+export const handleMountMultiplicationBlock = () => {
   const node_multiplication = LiteGraph.createNode('basic/multiplication'); // Создаём узел, который отображает входные данные
   graph.add(node_multiplication); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };
 
 // Деление
-export const handleMountDivisionBlock = (closeMenu) => {
+export const handleMountDivisionBlock = () => {
   const node_division = LiteGraph.createNode('basic/division'); // Создаём узел, который отображает входные данные
   graph.add(node_division); // Добавляем этот узел в графы, которые отображаются на холсте
-  closeMenu();
 };

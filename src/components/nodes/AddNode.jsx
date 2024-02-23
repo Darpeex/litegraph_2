724e16abbf948a6fad2d9275ee0546014740b88b
Darpeex <foxyyysp@mail.ru>
2024-02-23 11:41:10 +0300

    perf: развлекаюсь с рисунками поверх узлов

--- src/components/nodes/AddNode.jsx
+++ src/components/nodes/AddNode.jsx
@@ -27,6 +27,19 @@ AddNode.prototype.onExecute = function () {
   this.setOutputData(0, A + B);
 };
 
+// рисуем смайлик поверх узла
+AddNode.prototype.onDrawBackground = function (ctx) {
+  ctx.beginPath();
+  ctx.arc(75, 75, 50, 0, Math.PI * 2, true); // Внешняя окружность
+  ctx.moveTo(110, 75);
+  ctx.arc(75, 75, 35, 0, Math.PI, false); // рот (по часовой стрелке)
+  ctx.moveTo(65, 65);
+  ctx.arc(60, 65, 5, 0, Math.PI * 2, true); // Левый глаз
+  ctx.moveTo(95, 65);
+  ctx.arc(90, 65, 5, 0, Math.PI * 2, true); // Правый глаз
+  ctx.stroke();
+};
+
 //зарегистрировать в системе
 LiteGraph.registerNodeType('basic/addition', AddNode);
 

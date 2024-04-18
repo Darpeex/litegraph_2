973f4fcf36426f017a39e99f9eacbca39fecc682
Darpeex <foxyyysp@mail.ru>
2024-04-18 14:47:56 +0300

    refactor: задел под схемы

--- models/sheme.js
+++ models/sheme.js
@@ -1,85 +1,15 @@
 const mongoose = require('mongoose'); // нужна для создании схем
-const validator = require('validator'); // библиотека для валидации данных
 
 // Создаём схему и задаём её поля
 const shemeSchema = new mongoose.Schema(
   {
-    country: {
-      // страна создания схемы
+    shemeData: {
       type: String,
-      // при не соответствии условиям в [] - выдаются ошибки
-      required: [true, 'Поле "country" должно быть заполнено'],
-    },
-    director: {
-      // режиссёр схемыа
-      type: String,
-      required: [true, 'Поле "director" должно быть заполнено'],
-    },
-    duration: {
-      // длительность схемыа
-      type: Number,
-      required: [true, 'Поле "duration" должно быть заполнено'],
-    },
-    year: {
-      // год выпуска схемыа
-      type: String,
-      required: [true, 'Поле "year" должно быть заполнено'],
-    },
-    description: {
-      // описание схемыа
-      type: String,
-      required: [true, 'Поле "description" должно быть заполнено'],
-    },
-    image: {
-      // ссылка на постер к схемыу
-      type: String,
-      required: [true, 'Поле "image" должно быть заполнено'],
-      validate: {
-        // проверка на соответствие url
-        validator: (value) => validator.isURL(value),
-        message: 'Некорректный URL',
-      },
-    },
-    trailerLink: {
-      // ссылка на трейлер схемыа
-      type: String,
-      required: [true, 'Поле "trailerLink" должно быть заполнено'],
-      validate: {
-        // проверка на соответствие url
-        validator: (value) => validator.isURL(value),
-        message: 'Некорректный URL',
-      },
-    },
-    thumbnail: {
-      // миниатюрное изображение постера к схемыу
-      type: String,
-      required: [true, 'Поле "thumbnail" должно быть заполнено'],
-      validate: {
-        // проверка на соответствие url
-        validator: (value) => validator.isURL(value),
-        message: 'Некорректный URL',
-      },
-    },
-    owner: {
-      // _id пользователя, который сохранил схемы.
-      type: mongoose.Schema.Types.ObjectId, // тип данных для работы с идентификаторами(ObjectId)
-      required: [true, 'Поле "owner" должно быть заполнено'],
-      ref: 'User',
+      required: [true, 'Данные JSON схемы не переданы'],
     },
     shemeId: {
-      // id схемы, который содержится в ответе сервиса shemesExplorer
       type: Number,
-      required: [true, 'Поле "shemeId" должно быть заполнено'],
-    },
-    nameRU: {
-      // название схемы на русском языке
-      type: String,
-      required: [true, 'Поле "nameRU" должно быть заполнено'],
-    },
-    nameEN: {
-      // название схемы на английском языке
-      type: String,
-      required: [true, 'Поле "nameEN" должно быть заполнено'],
+      required: [true, 'Id схемы не передан'],
     },
   },
   { versionKey: false }, // убирает поле '__v' из ответа
--- routes/shemes.js
+++ routes/shemes.js
@@ -2,24 +2,12 @@ const { celebrate, Joi } = require('celebrate'); // библиотека для 
 const router = require('express').Router(); // создание нового экземпляра маршрутизатора вместо app
 const { getShemes, createSheme, deleteSheme } = require('../controllers/shemes'); // контроллеры
 
-const RegExp = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/;
-
 router.get('/shemes', getShemes); // возвращает все схемы
 router.post(
   '/shemes',
   celebrate({
     // создаёт схему
     body: Joi.object().keys({
-      country: Joi.string().required(),
-      director: Joi.string().required(),
-      duration: Joi.number().required(),
-      year: Joi.string().required(),
-      description: Joi.string().required(),
-      image: Joi.string().pattern(RegExp).required(),
-      trailerLink: Joi.string().pattern(RegExp).required(),
-      nameRU: Joi.string().required(),
-      nameEN: Joi.string().required(),
-      thumbnail: Joi.string().pattern(RegExp).required(),
       shemeId: Joi.number().integer().required(),
     }),
   }),

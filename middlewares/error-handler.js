de678bf874f648eede837a2599daa6804df9ee5a
Darpeex <foxyyysp@mail.ru>
2024-04-18 14:27:58 +0300

    refactor: задел под схемы

--- middlewares/error-handler.js
+++ middlewares/error-handler.js
@@ -1,13 +1,14 @@
-const errorHandler = (err, res, next) => { // здесь обрабатываем все ошибки
+const errorHandler = (err, res, next) => {
+  // здесь обрабатываем все ошибки
   const { statusCode = 500, message } = err;
 
-  res
-    .status(statusCode)
-    .send({ // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
+  res.status(statusCode).send({
+    // проверяем статус и выставляем сообщение в зависимости от него
+    message:
+      statusCode === 500
         ? 'На сервере произошла ошибка' // если статус кода 500
         : message, // иначе сообщение пришедшей ошибки
-    });
+  });
   return next();
 };
 
--- routes/shemes.js
+++ routes/shemes.js
@@ -1,29 +1,40 @@
 const { celebrate, Joi } = require('celebrate'); // библиотека для валидации данных
 const router = require('express').Router(); // создание нового экземпляра маршрутизатора вместо app
-const { getMovies, createMovie, deleteMovie } = require('../controllers/movies'); // контроллеры
+const { getShemes, createSheme, deleteSheme } = require('../controllers/shemes'); // контроллеры
 
 const RegExp = /^(http|https):\/\/(www\.)?[a-zA-Z0-9\--._~:/?#[\]@!$&'()*+,;=]+#?$/;
 
-router.get('/movies', getMovies); // возвращает все схемы
-router.post('/movies', celebrate({ // создаёт схему
-  body: Joi.object().keys({
-    country: Joi.string().required(),
-    director: Joi.string().required(),
-    duration: Joi.number().required(),
-    year: Joi.string().required(),
-    description: Joi.string().required(),
-    image: Joi.string().pattern(RegExp).required(),
-    trailerLink: Joi.string().pattern(RegExp).required(),
-    nameRU: Joi.string().required(),
-    nameEN: Joi.string().required(),
-    thumbnail: Joi.string().pattern(RegExp).required(),
-    movieId: Joi.number().integer().required(),
+router.get('/shemes', getShemes); // возвращает все схемы
+router.post(
+  '/shemes',
+  celebrate({
+    // создаёт схему
+    body: Joi.object().keys({
+      country: Joi.string().required(),
+      director: Joi.string().required(),
+      duration: Joi.number().required(),
+      year: Joi.string().required(),
+      description: Joi.string().required(),
+      image: Joi.string().pattern(RegExp).required(),
+      trailerLink: Joi.string().pattern(RegExp).required(),
+      nameRU: Joi.string().required(),
+      nameEN: Joi.string().required(),
+      thumbnail: Joi.string().pattern(RegExp).required(),
+      shemeId: Joi.number().integer().required(),
+    }),
   }),
-}), createMovie);
-router.delete('/movies/:movieId', celebrate({ // удаляет схему по идентификатору
-  params: Joi.object().keys({ // проверяет req.params на соответсвие
-    movieId: Joi.string().length(24).hex().required(), // hex() - от 0 до 9 и букв от A до F
+  createSheme,
+);
+router.delete(
+  '/shemes/:shemeId',
+  celebrate({
+    // удаляет схему по идентификатору
+    params: Joi.object().keys({
+      // проверяет req.params на соответсвие
+      shemeId: Joi.string().length(24).hex().required(), // hex() - от 0 до 9 и букв от A до F
+    }),
   }),
-}), deleteMovie);
+  deleteSheme,
+);
 
 module.exports = router;
